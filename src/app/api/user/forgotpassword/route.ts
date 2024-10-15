1cdc7cdadc28174ca079168b297fd03309bbadf9
Mushfiqur Niaz <mushfiqurniaz33@gmail.com>
2024-10-16 02:03:47 +0600

    added forgot password route

---
+++ src/app/api/user/forgotpassword/route.ts
@@ -0,0 +1,52 @@
+import DBConn from '@/lib/DBConn';
+import { verifyToken } from '@/lib/jwt';
+import sendMail from '@/lib/nodemailer';
+import userModel from '@/models/userModel';
+import { NextResponse } from 'next/server';
+
+export const POST = async (req: Request) => {
+  const verifyUser = verifyToken(process.env.JWT_SECRET as string, req);
+
+  if (!verifyUser) {
+    return NextResponse.json(
+      {
+        state: 'error',
+        message: "Can't reset password, login first.",
+      },
+      {
+        status: 401,
+      },
+    );
+  }
+  const userId = verifyUser.id;
+
+  try {
+    await DBConn();
+
+    const foundUser = await userModel.findById(userId);
+
+    await sendMail(foundUser.email);
+
+    return NextResponse.json(
+      {
+        state: 'error',
+        message:
+          'Enter code to update your password, code has been sent to your email.',
+      },
+      {
+        status: 200,
+      },
+    );
+  } catch (error) {
+    console.error('');
+    return NextResponse.json(
+      {
+        state: 'error',
+        message: 'Something went wrong.',
+      },
+      {
+        status: 500,
+      },
+    );
+  }
+};
