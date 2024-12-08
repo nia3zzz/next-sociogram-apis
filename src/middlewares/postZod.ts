6c2c3f1c01e2ee32f90844c6c46c5b280c490f72
Mushfiqur Niaz <mushfiqurniaz33@gmail.com>
2024-12-08 18:26:14 +0600

    added likepost route

--- src/middlewares/postZod.ts
+++ src/middlewares/postZod.ts
@@ -1,11 +1,11 @@
 import { z } from 'zod';
 
 const createPost = z.object({
-  caption: z
-    .string()
-    .min(1, 'Caption cannot be empty.')
-    .optional()
-    .nullable(),
+  caption: z.string().min(1, 'Caption cannot be empty.').optional().nullable(),
 });
 
-export { createPost };
+const reactPost = z.object({
+  postId: z.string().min(24, 'Invalid id format.'),
+});
+
+export { createPost, reactPost };
